02324cc49c160e8e92078d155dc9c561098b8f39
Eliel Miranda <eliel.miranda07@gmail.com>
2025-02-19 00:01:59 -0300

    19-02

--- js/inicio.js
+++ js/inicio.js
@@ -1,8 +1,11 @@
 // Header "contacto" deja de ser sticky
 const stickyElement = document.getElementById("divContacto");
 
+// Convierte 1200px a vh (ejemplo: si 1vh = 10px, entonces 1200px = 120vh)
+const scrollLimitVH = 150; // Ajusta este valor según lo necesites
+
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 1200) {
+  if (window.scrollY > (scrollLimitVH * window.innerHeight) / 100) {
     stickyElement.classList.add("hidden"); // Ocultar suavemente
   } else {
     stickyElement.classList.remove("hidden"); // Mostrar suavemente
